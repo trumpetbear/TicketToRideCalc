3a9d5f4d1b2f21d794bb9c37ad8fb1c272799a0b
trumpetbear <allison.ryank@gmail.com>
2018-04-26 18:24:36 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,52 +1,66 @@
-var p0 = ["Green", 0, 45];
-var p1 = ["Yellow", 0, 45];
-var p2 = ["Black", 0, 45];
-var p3 = ["Blue", 0, 45];
-var p4 = ["Red", 0, 45];
+//var player = [color, score, trains remaining, highlight color]
+var p0 = ["Green", 0, 45, '38ce21'];
+var p1 = ["Yellow", 0, 45, 'fcf93a'];
+var p2 = ["Black", 0, 45, '000000'];
+var p3 = ["Blue", 0, 45, '002eff'];
+var p4 = ["Red", 0, 45, 'ff0000'];
 var activePlayerNum = 0;
 var activePlayer = eval("p" + activePlayerNum);
 
 $(document).ready(function() {
     $('map').imageMapResize();
-	var color = '38ce21';
+	var color = activePlayer[3];
 
     $("map area").click(function(sender) {
         var raw = $(sender.target);
         var routenum = raw.attr("title");		
 		var route = eval("r" + routenum);
-		if (route[1]) {
-			ClaimDoubleRoute(route, this);
+		if (route[0] > activePlayer[2]) {
+			//$(this).data('maphilight', {alwaysOn:true, fillColor:color, fillOpacity: 0, strokeOpacity:0, strokeWidth:0}).trigger('alwaysOn.maphilight');
 		} else {
-			ClaimSingleRoute(route, this);
+			if (route[1]) {
+				ClaimDoubleRoute(route, this);
+			} else {
+				ClaimSingleRoute(route, this);
+			}
 		}
     });
 	
-	function ClaimSingleRoute(route, maparea){
+	function ClaimSingleRoute(route, maparea) {
 		if (route[2] == -1) {
-			var score = ScoreLookup(route[0]);
-			route[2] = activePlayerNum;
-			addScore(score);
-			subtractTrains(route);
-			$(maparea).data('maphilight', {alwaysOn:true, fillColor:color, strokeOpacity:0, strokeWidth:0}).trigger('alwaysOn.maphilight');
-		} else if (route[2] == activePlayerNum){
-			
+			ClaimRoute(route, maparea, false);
 		} else {
+			if (route[2] == activePlayerNum) {
 				
+			}
 		}
 	}
 	
 	function ClaimDoubleRoute(route, maparea) {
-		if (route[2] == -1) {
-			var score = ScoreLookup(route[0]);
+		if (route[2] == -1 && route[3] == -1) {
+			ClaimRoute(route, maparea, false);
+		} else if (route[3] == -1){
+			if (route[2] != activePlayerNum) {
+				ClaimRoute(route, maparea, true);
+			}
+		}
+	}
+	
+	function ClaimRoute(route, maparea, claimsecondspot){
+		var score = ScoreLookup(route[0]);		
+		addScore(score);
+		subtractTrains(route);
+		if (claimsecondspot) {
+			route[3] = activePlayerNum;
+			$(maparea).data('maphilight', {alwaysOn:true, fillColor:eval("p"+route[2])[3], fillOpacity:1, strokeOpacity:1, strokeWidth:4, strokeColor:color}).trigger('alwaysOn.maphilight');
+		} else {
 			route[2] = activePlayerNum;
-			addScore(score);
-			subtractTrains(route);
-			$(maparea).data('maphilight', {alwaysOn:true, fillColor:color, strokeOpacity:0, strokeWidth:0}).trigger('alwaysOn.maphilight');
+			$(maparea).data('maphilight', {alwaysOn:true, fillColor:color, fillOpacity:1, strokeOpacity:0, strokeWidth:0}).trigger('alwaysOn.maphilight');
 		}
 	}
 
     $('img[usemap]').maphilight({
-        fillOpacity: 1,
+        fillOpacity: 0,
         strokeOpacity: 0
     });
 
@@ -60,109 +74,129 @@ $(document).ready(function() {
 
         switch (lnkId) {
             case "spnGreen":
-                color = '38ce21';
+                color = p0[3];
 				activePlayerNum = 0;
                 break;
             case "spnYellow":
-				color = 'fcf93a';
+				color = p1[3];
 				activePlayerNum = 1;
                 break;
             case "spnBlack":
-				color = '000000';
+				color = p2[3];
 				activePlayerNum = 2;
                 break;
             case "spnBlue":
-				color = '002eff';
+				color = p3[3];
 				activePlayerNum = 3;
                 break;
 			case "spnRed":
-				color = 'ff0000';
+				color = p4[3];
 				activePlayerNum = 4;
                 break;
         }
 		activePlayer = eval("p" + activePlayerNum);
     });
+	
+	$("#divClear").click(function() {
+		//Reset Route Data
+		for (i = 1; i < 78; i++){
+			var rt = eval("r" + i);
+			rt[2] = -1;
+			rt[3] = -1;
+		}
+		//Reset Player Data
+		p0 = ["Green", 0, 45, '38ce21'];
+		p1 = ["Yellow", 0, 45, 'fcf93a'];
+		p2 = ["Black", 0, 45, '000000'];
+		p3 = ["Blue", 0, 45, '002eff'];
+		p4 = ["Red", 0, 45, 'ff0000'];
+		//Reset Score Display
+		$(".txt").html("0");
+		$(".rem").html("45");
+		//Reset Route Display
+		$('img[usemap]').maphilight({fillOpacity: 0,strokeOpacity: 0});
+	});
 });
 
-//Var routeNumber = [routeLength, allowDoubleRoute,routeIsClaimedBy, routeisAlsoClaimedBy];
-var r1 = [3, false, -1, -1];
-var r2 = [6, false, -1, -1];
-var r3 = [6, false, -1, -1];
-var r4 = [5, false, -1, -1];
+//Var routeNumber = [routeLength, allowDoubleRoute, routeIsClaimedBy, routeisAlsoClaimedBy];
+var r1 = [3, false, -1];
+var r2 = [6, false, -1];
+var r3 = [6, false, -1];
+var r4 = [5, false, -1];
 var r5 = [2, true, -1, -1];
 var r6 = [1, true, -1, -1];
-var r7 = [4, false, -1, -1];
-var r8 = [4, false, -1, -1];
-var r9 = [4, false, -1, -1];
-var r10 = [4, false, -1, -1];
-var r11 = [3, false, -1, -1];
-var r12 = [2, false, -1, -1];
-var r13 = [3, false, -1, -1];
-var r14 = [3, false, -1, -1];
+var r7 = [4, false, -1];
+var r8 = [4, false, -1];
+var r9 = [4, false, -1];
+var r10 = [4, false, -1];
+var r11 = [3, false, -1];
+var r12 = [2, false, -1];
+var r13 = [3, false, -1];
+var r14 = [3, false, -1];
 var r15 = [1, true, -1, -1];
-var r16 = [6, false, -1, -1];
-var r17 = [6, false, -1, -1];
-var r18 = [6, false, -1, -1];
+var r16 = [6, false, -1];
+var r17 = [6, false, -1];
+var r18 = [6, false, -1];
 var r19 = [2, true, -1, -1];
 var r20 = [5, true, -1, -1];
-var r21 = [6, false, -1, -1];
-var r22 = [3, false, -1, -1];
-var r23 = [4, false, -1, -1];
-var r24 = [5, false, -1, -1];
+var r21 = [6, false, -1];
+var r22 = [3, false, -1];
+var r23 = [4, false, -1];
+var r24 = [5, false, -1];
 var r25 = [2, true, -1, -1];
-var r26 = [3, false, -1, -1];
-var r27 = [4, false, -1, -1];
-var r28 = [2, false, -1, -1];
+var r26 = [3, false, -1];
+var r27 = [4, false, -1];
+var r28 = [2, false, -1];
 var r29 = [5, true, -1, -1];
 var r30 = [3, true, -1, -1];
-var r31 = [4, false, -1, -1];
-var r32 = [4, false, -1, -1];
+var r31 = [4, false, -1];
+var r32 = [4, false, -1];
 var r33 = [3, true, -1, -1];
 var r34 = [2, true, -1, -1];
 var r35 = [2, true, -1, -1];
 var r36 = [3, true, -1, -1];
-var r37 = [2, false, -1, -1];
-var r38 = [3, false, -1, -1];
-var r39 = [5, false, -1, -1];
-var r40 = [2, false, -1, -1];
-var r41 = [4, false, -1, -1];
+var r37 = [2, false, -1];
+var r38 = [3, false, -1];
+var r39 = [5, false, -1];
+var r40 = [2, false, -1];
+var r41 = [4, false, -1];
 var r42 = [4, true, -1, -1];
 var r43 = [1, true, -1, -1];
 var r44 = [2, true, -1, -1];
 var r45 = [2, true, -1, -1];
-var r46 = [5, false, -1, -1];
-var r47 = [4, false, -1, -1];
-var r48 = [2, false, -1, -1];
-var r49 = [2, false, -1, -1];
-var r50 = [3, false, -1, -1];
-var r51 = [3, false, -1, -1];
-var r52 = [3, false, -1, -1];
+var r46 = [5, false, -1];
+var r47 = [4, false, -1];
+var r48 = [2, false, -1];
+var r49 = [2, false, -1];
+var r50 = [3, false, -1];
+var r51 = [3, false, -1];
+var r52 = [3, false, -1];
 var r53 = [2, true, -1, -1];
-var r54 = [2, false, -1, -1];
-var r55 = [2, false, -1, -1];
-var r56 = [3, false, -1, -1];
-var r57 = [2, false, -1, -1];
-var r58 = [1, false, -1, -1];
-var r59 = [3, false, -1, -1];
+var r54 = [2, false, -1];
+var r55 = [2, false, -1];
+var r56 = [3, false, -1];
+var r57 = [2, false, -1];
+var r58 = [1, false, -1];
+var r59 = [3, false, -1];
 var r60 = [2, true, -1, -1];
-var r61 = [6, false, -1, -1];
-var r62 = [3, false, -1, -1];
-var r63 = [2, false, -1, -1];
-var r64 = [5, false, -1, -1];
+var r61 = [6, false, -1];
+var r62 = [3, false, -1];
+var r63 = [2, false, -1];
+var r64 = [5, false, -1];
 var r65 = [2, true, -1, -1];
-var r66 = [2, false, -1, -1];
-var r67 = [3, false, -1, -1];
+var r66 = [2, false, -1];
+var r67 = [3, false, -1];
 var r68 = [4, true, -1, -1];
 var r69 = [2, true, -1, -1];
-var r70 = [2, false, -1, -1];
-var r71 = [2, false, -1, -1];
-var r72 = [6, false, -1, -1];
-var r73 = [4, false, -1, -1];
+var r70 = [2, false, -1];
+var r71 = [2, false, -1];
+var r72 = [6, false, -1];
+var r73 = [4, false, -1];
 var r74 = [1, true, -1, -1];
-var r75 = [2, false, -1, -1];
-var r76 = [6, false, -1, -1];
-var r77 = [5, false, -1, -1];
-var r78 = [4, false, -1, -1];
+var r75 = [2, false, -1];
+var r76 = [6, false, -1];
+var r77 = [5, false, -1];
+var r78 = [4, false, -1];
 
 function ScoreLookup(rtLen) {
     switch (rtLen) {
